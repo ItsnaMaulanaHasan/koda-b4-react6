f0f6dcaeca2576d7d4abb6b6773029865a509dbc
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-02 15:14:15 +0700

    feat: create responsiveness card article

--- src/components/CardArticle.jsx
+++ src/components/CardArticle.jsx
@@ -2,13 +2,19 @@ import moment from "moment";
 
 function CardArticle({ articleData }) {
   return (
-    <div className="flex flex-col gap-3">
-      <img className="w-full h-1/2" src={articleData.image} alt={articleData.creator.name} />
+    <div className="flex flex-col h-full gap-3">
+      <div className="w-full h-[200px] overflow-hidden">
+        <img className="w-full h-full object-cover" src={articleData.image} alt={articleData.title} />
+      </div>
       <h1 className="font-semibold text-4xl">{articleData.title}</h1>
-      <p className="line-clamp-3">{articleData.content}</p>
+      <p className="line-clamp-2 sm:line-clamp-3 text-sm sm:text-base text-gray-600">{articleData.content}</p>
       <div className="flex gap-2 items-center text-[#6B6B6B] text-sm">
-        <img className="rounded-full overflow-hidden h-5 w-5" src={articleData.creator.avatar} alt={articleData.creator.name} />
-        <div>{articleData.creator.name}</div>•<div>{moment(articleData.publishedAt).format("MMM DD")}</div>•<div>{articleData.readTime} min read</div>
+        <img className="rounded-full h-5 w-5 object-cover" src={articleData.creator.avatar} alt={articleData.creator.name} />
+        <span>{articleData.creator.name}</span>
+        <span>•</span>
+        <span>{moment(articleData.publishedAt).format("MMM DD")}</span>
+        <span>•</span>
+        <span>{articleData.readTime} min read</span>
       </div>
     </div>
   );
