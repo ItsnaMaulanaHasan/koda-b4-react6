125a4eb50bf1b445eea8506abafd91ed527d80f5
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-10-03 11:29:55 +0700

    feat: create layout with outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Article from "./pages/Article";
+import SearchPage from "./pages/SearchPage";
+import MainLayout from "./components/layouts/MainLayout";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <HomePage />,
-    },
-    {
-      path: "/:creator/:slug",
-      element: <Article />,
+      element: <MainLayout />,
+      children: [
+        {
+          path: "",
+          element: <HomePage />,
+        },
+        {
+          path: "/:creator/:slug",
+          element: <Article />,
+        },
+        {
+          path: "/search",
+          element: <SearchPage />,
+        },
+      ],
     },
   ]);
   return <RouterProvider router={router} />;
